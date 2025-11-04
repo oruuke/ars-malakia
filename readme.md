2ab637df451baa0abd539038585327c54e446e00
oruuke <goober@oruuke.com>
2025-11-04 19:59:45 +1000

    is a weeny bit cucked but fulfilled a tickbox and took me too long to land on dis library so yea

--- readme.md
+++ readme.md
@@ -3,6 +3,6 @@
 ## todo
 - [x] figure out ratatui
 - [x] build out elm architecture
-- [ ] add syntax highlighting for code examples
+- [x] add syntax highlighting for code examples
 - [ ] add code sandbox option for code examples
 - [ ] add searching
--- src/view.rs
+++ src/view.rs
@@ -1,3 +1,12 @@
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{Terminal, backend::CrosstermBackend, layout::Position};
+use ratatui_code_editor::editor::Editor;
+use std::io::stdout;
+
 use crate::model::Model;
 use ratatui::{
     Frame,
@@ -6,10 +15,19 @@ use ratatui::{
 };
 
 // rendering view to always produce same ui representation for given model
-pub fn view(model: &mut Model, frame: &mut Frame) {
+pub fn view(_model: &mut Model, frame: &mut Frame) {
+    let theme = vec![
+        ("keyword", "#ff6b6b"),
+        ("string", "#4ecdc4"),
+        ("comment", "#95a5a6"),
+        ("function", "#f39c12"),
+        ("variable", "#B55088"),
+        ("namespace", "#eb34de"),
+        ("type", "#4ecdc4"),
+    ];
+    let content = "fn main() {\n    println!(\"Hello, world!\");\n}";
+    let mut editor = Editor::new("rust", content, theme);
+
     let block = Block::bordered().border_set(border::THICK);
-    let p = Paragraph::new(format!("counter: {}", model.counter))
-        .centered()
-        .block(block);
-    frame.render_widget(p, frame.area());
+    frame.render_widget(&editor, frame.area());
 }
